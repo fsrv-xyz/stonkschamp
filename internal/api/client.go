6b19db28527c9b7c754f65aca4f5a4cea037a5f5
Florian Bauer <florian@fsrv.xyz>
2022-11-13 14:11:56 +0100

    chore: use http.MethodGet instead of "GET"

--- internal/api/client.go
+++ internal/api/client.go
@@ -19,7 +19,7 @@ func (c *Client) GetPerformanceInformation(ISIN string) (PerformanceInformation,
 	c.URL.RawQuery = "organization=PCPK21&api-version=beta-1.0.0"
 	uri := c.URL.JoinPath("beta/web/calc/union-tools-pcpk/public/product/performanceInformation").JoinPath(ISIN).String()
 
-	request, _ := http.NewRequest("GET", uri, nil)
+	request, _ := http.NewRequest(http.MethodGet, uri, nil)
 	request.Header.Set("x-api-key", c.Key)
 	request.Header.Set("Accept", "application/json")
 	request.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15")
